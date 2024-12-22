49befa9533607e32b066c257733d8227cdaf2c41
Mahmoud Nabil <mahmoudnabil141981@gmail.com>
2024-12-22 13:45:50 +0200

    - format connectivity_monitor.dart

--- test/connectivity_monitor_test.dart
+++ test/connectivity_monitor_test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:connectivity_monitor/src/connectivity_monitor.dart';
-
 void main() {
-  test('adds one to input values', () {
-  });
+  test('adds one to input values', () {});
 }
